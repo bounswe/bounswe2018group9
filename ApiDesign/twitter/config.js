fd2143b30e8c0c1886d957f28efc8d7eb30b333b
kemaltulum <tulum.ke@gmail.com>
2018-04-03 23:13:08 +0300

    api(npm): NPM project initialization is done

---
+++ ApiDesign/twitter/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key:         'BHaUU9kMXdMQ62VY87y1jeEo9',
+    consumer_secret:      'QTkLGQQYORBvvkocHOYn8jRcmV0loJyVFXdEOJwIvEzarldkHz',
+    access_token:         '901503538676805632-bDJ2UFLBhqmBfHpbnZx57WAOnZd7zTT',
+    access_token_secret:  'cG9ehnCcLXTqtdZCmC8hANra32zuguRfSC9JZD97zGEtR'
+    //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+};
