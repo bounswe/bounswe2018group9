e56b67cc8ef1c3c1be3495526b14d5008e82a487
nazmicancalik <nazmicancalik@gmail.com>
2018-04-12 03:01:31 +0300

    trends(api): Get trending topic names and url's for given country endpoint.

--- ApiDesign/index.js
+++ ApiDesign/index.js
@@ -4,40 +4,45 @@ const twitterAPI = require('./twitter');
 const app = express();
 const port = 3000;
 
-app.get('/getTweets', (request, response) => {
+app.get('/getTweets/:contains', (request, response) => {
     console.log(request);
-    twitterAPI.basics.getTweetsContaining('yeşilay', 3, (error, data, twitterRes) => {
+    let contains = request.params.contains; 
+    twitterAPI.basics.getTweetsContaining(contains, 3, (error, data, twitterRes) => {
         if (data != null) {
             const tweets = data.statuses;
             const responseArray = [];
-            for (let i=0; i<tweets.length; i++){
+            for (let i = 0; i < tweets.length; i++) {
                 console.log(tweets[i].text);
                 responseArray.push(tweets[i].text);
             }
             response.end(JSON.stringify(responseArray));
-        }else {
+        } else {
             response.end('No tweet is retrieved');
         }
     });
 });
 
-app.get('/getTrends/:id',(request,response) => {
+app.get('/getTrends/:countryName', (request, response) => {
     console.log(request);
-    twitterAPI.trends.getTrendsForCity(id,(error,data,twitterRes)=>{
-        if(data != null){
-            const tweets = data.statuses;
-            const responseArray = [];
-            for(let tweet of tweets){
-                console.log(tweet.text);
-                responseArray.push(tweet.text);
+    let countryName = request.params.countryName; 
+    twitterAPI.trends.getTrendsForCountry(countryName, (error, data, twitterRes) => {
+        if (data != null) {
+            const trends = data[0].trends;
+            const res = [];
+            for(let trend of trends){
+                tempTrend = {
+                    "name":trend.name,
+                    "url":trend.url
+                };
+                res.push(tempTrend);
             }
-            response.end(JSON.stringify(responseArray));
-        }else{
+            response.end(JSON.stringify(res));
+        } else {
             response.end('No trend is retrieved');
         }
     });
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Server is running on ' + port);
 });
