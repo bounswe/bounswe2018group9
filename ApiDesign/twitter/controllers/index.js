723a6901c4611b4c48992e0166ae6265fcb38c36
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-04-13 11:33:46 +0300

    refactor(api): merge basics and tweets modules

--- ApiDesign/twitter/controllers/index.js
+++ ApiDesign/twitter/controllers/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const Basics = require('./basics');
 const Trends = require('./trends');
 const Tweets = require('./tweets');
 
-// Basics module router
-router.get('/tweets/:contains', Basics.getTweetsContaining);
+// Tweets module router
+router.get('/tweets/:contains', Tweets.getTweetsContaining);
+router.post('/tweets', Tweets.postTweet);
+router.get('/home', Tweets.getHome);
 
 // Trends module router
 router.get('/trends/:countryName', Trends.getTrendsForCountry);
-
-// Tweets module router
-router.post('/tweets', Tweets.postTweet);
-router.get('/home', Tweets.getHome);
 
 module.exports = router;
