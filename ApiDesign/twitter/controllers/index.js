2cdded6af56ca38acda07478a6eccb5cebccda7a
Serkan Özel <32355782+serkan-ozel-2015400123@users.noreply.github.com>
2018-04-14 13:19:07 +0300

    Retweet (#116)
    * add retweet controller and module
    
    * retweet(api) add retweet endpoint
    
    * refactor(retweet): merge retweetjs into tweetjs
    
    * feat(api): add retweet method under tweets.js
    
    * feat(geo): add geo.js

--- ApiDesign/twitter/controllers/index.js
+++ ApiDesign/twitter/controllers/index.js
@@ -3,13 +3,15 @@ const router = express.Router();
 
 const Trends = require('./trends');
 const Tweets = require('./tweets');
-
+const Geo = require('./geo');
 // Tweets module router
 router.get('/tweets', Tweets.getTweetsContaining);
 router.post('/tweets', Tweets.postTweet);
 router.get('/home', Tweets.getHome);
-
+router.post('/retweet/:id',Tweets.retweet);
 // Trends module router
 router.get('/trends/:countryName', Trends.getTrendsForCountry);
+// Geo module router
+router.get('geo/search',Geo.getInfoAboutPlace)
 
 module.exports = router;
