b8c83769b85e511d76dc3af84e4c9273db95f63b
Nazmican <nazmicancalik@gmail.com>
2018-10-08 14:58:28 +0300

    Nodemon is added, cleanup.

--- backend/app.js
+++ backend/app.js
@@ -2,20 +2,22 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 
+// Import routers. 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Register routers.
+// Register static angular files endpoint.
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// Register API routers.
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,7 +32,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Welcome to the homepage, angular will be served from here. For API Endpoints please refer to /api/COLLECTION_NAME');
 });
 
 module.exports = router;
