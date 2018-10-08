9bca14ea90ac0f34078c17997b0d2f076d62ab92
Nazmican <nazmicancalik@gmail.com>
2018-10-08 14:58:28 +0300

    Nodemon is added, cleanup.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users listing. Starts after /users */
 router.get('/', function(req, res, next) {
-  res.send('User collection handlers');
+  res.send('Userlar burda abi');
 });
 
 module.exports = router;
