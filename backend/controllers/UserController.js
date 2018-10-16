f06bc533c1250283e1053bb2461cda38ada452e3
Nazmican <nazmicancalik@gmail.com>
2018-10-16 19:17:57 +0300

    User model and controller created. Auth route is added

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,2 +1,21 @@
-// Controllers will be registered here. 
-// These are the main classes that manipulate the data.
+var mongoose = require("mongoose");
+var User = require("../models/User");
+
+exports.addUser = function(req,res,next) {
+    var user = new User({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password
+    });
+
+    // Before save salt the password
+    
+    user.save(function(err,user){
+        console.log('Error var');
+        if(!err){
+            console.log('Error yok');
+            res.send(user);
+        }
+    });
+};
--- backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. Starts after /users */
-router.get('/', function(req, res, next) {
-  res.send('Userlar burda abi');
-});
-
-module.exports = router;
