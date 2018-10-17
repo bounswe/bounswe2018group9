c5d63f1cd0be9ca69dbeba68772b2c213b693537
kemaltulum <tulum.ke@gmail.com>
2018-10-17 17:31:02 +0300

    Ionic project setup

---
+++ frontend/src/app/event/event.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.page.html',
+  styleUrls: ['./event.page.scss'],
+})
+export class EventPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
