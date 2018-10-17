1febb6a6290d2b04b90d08a53198d7f0b2c04e5b
Yusuf Kalaycı <byklyci32@gmail.com>
2018-10-17 20:55:01 +0300

    sign in page created and working

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -8,7 +8,7 @@ exports.addUser = function(req,res,next) {
         lastName: req.body.lastName,
         password: req.body.password
     });
-
+    console.log(JSON.stringify(req.body,null,2));
     // Before save salt the password
     
     user.save(function(err,user){
@@ -18,4 +18,22 @@ exports.addUser = function(req,res,next) {
             res.send(user);
         }
     });
+};
+//When user sign in it controls email and password true or not 
+exports.signUser = function(req,res,next){
+    console.log('body', req.body);
+         var email = req.body.email;
+         var password = req.body.password;
+         console.log('email', email);
+         console.log('pass', password);
+         User.findOne({email:email,password:password}, function(err,user){
+             if(err){
+                 console.log(err);
+                 return res.status(500).send();
+             }
+             if(!user){
+                 return res.status(404).send();
+             }
+             return res.status(200).send();
+         })
 };
