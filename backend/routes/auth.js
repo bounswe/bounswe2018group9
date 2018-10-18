2b99384b744c52580a17ec577ef438669e892c22
Yusuf Kalaycı <byklyci32@gmail.com>
2018-10-18 13:58:29 +0100

    f-signin first interim  (#169)
    * sign in page created and working
    
    * sign in page created and it controls the user sign up or not

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,8 @@ var UserController = require('../controllers/UserController');
 // Import model
 var User = require('../models/User');
 
+// Router from UserController
 router.post('/signup', UserController.addUser);
+router.post('/signin', UserController.signUser);
 
 module.exports = router;
