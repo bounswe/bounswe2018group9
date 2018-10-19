6eabde72eaea5374c615fa46357612166fa42d46
kemaltulum <tulum.ke@gmail.com>
2018-10-19 13:05:41 +0300

    fronent(http): Add HttpClient Module

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BackendService} from "../services/backend.service";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(private backend: BackendService){
+    this.backend.getEvents()
+      .subscribe(
+        data => console.log(data),
+        error => console.log(error)
+      )
+  }
+
+
 
 }
