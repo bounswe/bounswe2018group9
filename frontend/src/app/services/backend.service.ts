9dd70ca76415750858b7fe688f9ee5b1f90d3638
kemaltulum <tulum.ke@gmail.com>
2018-10-19 13:05:41 +0300

    fronent(http): Add HttpClient Module

--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from "@angular/common/http";
+
 import {Event} from "../../models/event.interface";
+import {Observable} from "rxjs/internal/Observable";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getEvents():Array<Event>{
-      return [];
+  getEvents():Observable<Array<Event>>{
+      return this.http.get<Array<Event>>('/resources/api/events.json');
   }
 }
