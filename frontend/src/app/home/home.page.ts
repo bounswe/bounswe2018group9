9b6d9761f49896189d1a5e44a0f212f87ae7d5db
Didem Öngü <didemongu@Didem-MacBook-Pro.local>
2018-10-19 13:58:59 +0300

    fronted(user) add user

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {BackendService} from "../services/backend.service";
+import { Event } from "../../models/event.interface";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  events: Array<Event>;
 
   constructor(private backend: BackendService){
+    
+  }
+
+  ngOnInit(){
     this.backend.getEvents()
       .subscribe(
-        data => console.log(data),
-        error => console.log(error)
-      )
+        (data) => {
+          console.log(data);
+          this.events = data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 
 
