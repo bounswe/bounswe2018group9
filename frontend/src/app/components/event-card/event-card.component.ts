73dcb0b3893574fd356f1437943a768897f3e6bd
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-10-25 14:51:46 +0300

    chore(scaffolding): utilize interface index file

--- frontend/src/app/components/event-card/event-card.component.ts
+++ frontend/src/app/components/event-card/event-card.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Event} from "../../interfaces/event.interface";
+import {Event} from "../../interfaces";
 
 @Component({
   selector: 'app-event-card',
---
+++ frontend/src/app/interfaces/index.ts
@@ -0,0 +1,2 @@
+export { User } from './user.interface';
+export { Event } from './event.interface';
