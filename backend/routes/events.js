b11987edceedc418d852716d6d37519609eb015e
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-10-25 23:56:36 +0300

    Missing semicolon added

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -9,8 +9,9 @@ var Event= require('../models/Event');
 
 router.post('/', EventController.addEvent);
 
-router.get('/', EventController.getAllEvents);
-router.get('/getEventbyOwner/:id', EventController.getEventbyOwner)
+
+router.get('/',EventController.getAllEvents);
+router.get('/getEventbyOwner/:id',EventController.getEventbyOwner);
 router.get('/:id', EventController.getEventbyId);//this should be last in code
 
 module.exports=router;
