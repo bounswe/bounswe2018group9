9c2f7f5052e5f1c8478c05c41781b0b424b9f364
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-10-25 14:39:17 +0300

    chore(scaffolding): cluster files according to functionality

--- frontend/src/app/pages/home/home.module.ts
+++ frontend/src/app/pages/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
-import { EventCardComponent } from '../components/event-card/event-card.component';
+import { EventCardComponent } from '../../components/event-card/event-card.component';
 
 @NgModule({
   imports: [
