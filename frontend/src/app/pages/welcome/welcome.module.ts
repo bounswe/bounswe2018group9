33e1edd22628b02d9ed8b6dadc3c94dfc86665bf
kemaltulum <tulum.ke@gmail.com>
2018-10-26 12:38:21 +0300

    feat(auth): add auth form templates

--- frontend/src/app/pages/welcome/welcome.module.ts
+++ frontend/src/app/pages/welcome/welcome.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+
 
 import { IonicModule } from '@ionic/angular';
 
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    ReactiveFormsModule
   ],
   declarations: [WelcomePage]
 })
