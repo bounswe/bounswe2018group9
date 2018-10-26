dd8285aba3aa099efc385e02dbfaa448646877ec
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-10-27 00:58:08 +0300

    feat(ui): seperate signin & signup pages

---
+++ frontend/src/app/pages/signin/signin.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
+import { AuthService } from '../../services';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.page.html',
+  styleUrls: ['./signin.page.scss'],
+})
+export class SigninPage implements OnInit {
+  private form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+    this.form = this.formBuilder.group(
+      {
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+      }
+    );
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.form.value);
+  }
+}
