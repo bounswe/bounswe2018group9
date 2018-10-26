9816df462639d166512ef96017caec26f5dbf571
kemaltulum <tulum.ke@gmail.com>
2018-10-26 12:38:21 +0300

    feat(auth): add auth form templates

--- frontend/src/app/pages/welcome/welcome.page.ts
+++ frontend/src/app/pages/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.page.scss'],
 })
 export class WelcomePage implements OnInit {
+  private signUpForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.signUpForm = this.formBuilder.group(
+      {
+        name: ['', Validators.required],
+        lastname: ['', Validators.required],
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+
+  signUp(){
+    console.log(this.signUpForm.value);
   }
 
 }
