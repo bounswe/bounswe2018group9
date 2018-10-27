ef8a0f9d4541a603a4ad116a7117a80def6dd05d
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-10-27 15:43:34 +0300

    fix(modules): correctly import JWTModule

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -5,7 +5,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { JwtModule } from '@auth0/angular-jwt';
+import { JwtModule, JwtInterceptor, JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { SigninPage } from './pages/signin/signin.page';
 import { SignupPage } from './pages/signup/signup.page'
@@ -19,27 +20,43 @@ import { environment } from '../../environments/environment';
     HttpClientModule,
     IonicModule,
     ReactiveFormsModule,
-    AuthRoutingModule,
-    JwtModule.forRoot({
-      config: {
-        tokenGetter: tokenGetter,
-        whitelistedDomains: [ environment.endpoint ]
-      }
-    }),
+    AuthRoutingModule
   ],
   declarations: [
     SigninPage,
     SignupPage
   ],
   providers: [
-    AuthService
+    AuthService,
+    // JWT Providers
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+    {
+      provide: JWT_OPTIONS,
+      useValue: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: [ environment.endpoint ]
+      }
+    },
+    JwtHelperService
   ]
 })
 export class AuthModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: AuthModule,
-      providers: [ AuthService ]
+      providers: [
+        AuthService,
+        // JWT Interceptor
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: JwtInterceptor,
+          multi: true
+        }
+      ]
     }
   }
 }
