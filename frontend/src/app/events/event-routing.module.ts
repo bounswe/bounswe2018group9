ac69a5f6efa95a1f37c50695879d51dc7b1a19d4
Serkan Özel <serkan.ozel@boun.edu.tr>
2018-10-28 14:22:46 +0300

    chore(event): remove duplicate route for event creation

--- frontend/src/app/events/event-routing.module.ts
+++ frontend/src/app/events/event-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { EventPage } from './pages/event/event.page';
 import { FeedPage } from './pages/feed/feed.page';
+import {EventCreatePage} from './pages/event-create/event-create.page';
 
 const routes: Routes = [
   { path: '', component: FeedPage },
-  { path: 'create', component: EventPage },
-  { path: ':id', component: EventPage },
-  { path: 'event-create', loadChildren: './pages/event-create/event-create.module#EventCreatePageModule' }
+  { path: 'create', component: EventCreatePage },
+  { path: ':id', component: EventPage }
 ];
 
 @NgModule({
