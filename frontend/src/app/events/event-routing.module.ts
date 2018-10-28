f467ce4853e06295b02536818e331fe54daa9226
Serkan Özel <serkan.ozel@boun.edu.tr>
2018-10-28 14:19:09 +0300

    chore(scaffolding): create event-create component

--- frontend/src/app/events/event-routing.module.ts
+++ frontend/src/app/events/event-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { EventPage } from './pages/event/event.page';
 import { FeedPage } from './pages/feed/feed.page';
+import { EventPage } from './pages/event/event.page';
+import { EventCreatePage } from './pages/event-create/event-create.page';
 
 const routes: Routes = [
   { path: '', component: FeedPage },
-  { path: 'create', component: EventPage },
-  { path: ':id', component: EventPage }
+  { path: 'create', component: EventCreatePage },
+  { path: ':id', component: EventPage },
 ];
 
 @NgModule({
