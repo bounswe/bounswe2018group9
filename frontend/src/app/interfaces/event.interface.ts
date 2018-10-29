1c375a33feffd8c0ed0c2977d80f5ec9f89ae05b
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-10-29 16:03:09 +0300

    feat(data): update event data model

--- frontend/src/app/interfaces/event.interface.ts
+++ frontend/src/app/interfaces/event.interface.ts
@@ -2,8 +2,10 @@ import { Object } from './object.interface';
 import {User} from "./user.interface";
 
 export interface Event extends Object {
-    name:string;
-    description: string;
-    user: User;
-    mainImage: string;
+  owner?: string;
+  name?: string;
+  description?: string;
+  artists?: string;
+  date?: number;
+  price?: number;
 }
