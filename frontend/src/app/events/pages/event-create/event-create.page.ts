67daaff8b75f077a2f66303b72f47d5702419e14
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-10-29 16:09:28 +0300

    feat(ui): add event create form

--- frontend/src/app/events/pages/event-create/event-create.page.ts
+++ frontend/src/app/events/pages/event-create/event-create.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-event-create',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-create.page.scss'],
 })
 export class EventCreatePage implements OnInit {
+ form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      artists: ['', Validators.required],
+      date: ['', Validators.required],
+      price: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
   }
 
+  createEvent() {
+
+  }
 }
