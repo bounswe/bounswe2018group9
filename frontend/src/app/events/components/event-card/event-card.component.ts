b649cf3cc1db5ec8c7e4f2841f2309a0774c18b3
kemaltulum <tulum.ke@gmail.com>
2018-10-29 22:24:51 +0300

    chore(event): Adjust event page nd event-card components

--- frontend/src/app/events/components/event-card/event-card.component.ts
+++ frontend/src/app/events/components/event-card/event-card.component.ts
@@ -9,9 +9,40 @@ import {Event} from "../../../interfaces";
 export class EventCardComponent implements OnInit {
   @Input('event') event: Event;
 
+  randomProfileImage: string;
+  likes: number;
+  comments: number;
+  hours: number;
+
+  date: Date;
+  date_str: string;
+
   constructor() { }
 
   ngOnInit() {
+    let url = 'https://randomuser.me/api/portraits/';
+
+    let gender = Math.random() > 0.5 ? 'men' : 'women';
+    let randNum = this.getRndInteger(1, 80);
+    this.randomProfileImage = url + gender + '/' + randNum + '.jpg';
+
+    this.likes = this.getRndInteger(1, 100);
+    this.comments = this.getRndInteger(1, 25);
+    this.hours = this.getRndInteger(1,24);
+
+    this.date = new Date(this.event.date);
+
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.date_str = this.date.getDate() + ' ' + months[this.date.getMonth()] + ' ' + this.date.getFullYear();
+
+    console.log(this.event);
+    console.log(this.date);
+
+  }
+
+  getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
   }
 
 }
--- frontend/src/app/events/pages/event/event.page.ts
+++ frontend/src/app/events/pages/event/event.page.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Event } from '../../../interfaces';
 import { ActivatedRoute } from '@angular/router';
 import { EventService } from '../../../data/providers/event/event.service';
+import {Observable} from "rxjs/internal/Observable";
 
 @Component({
   selector: 'app-event',
@@ -10,9 +11,7 @@ import { EventService } from '../../../data/providers/event/event.service';
 })
 export class EventPage implements OnInit, OnDestroy{
 
-  isDataAvailable = false;
-  eventPromise: Promise<Event>;
-  event: Event;
+  event: Observable<Event | Event[]>;
   private sub: any;
   event_id: string;
 
@@ -25,32 +24,19 @@ export class EventPage implements OnInit, OnDestroy{
     this.sub = this.route.params.subscribe(params => {
       if(params){
         this.event_id = params['id'];
-        console.log(this.event_id);
+        this.event = this.eventService.get(this.event_id);
       }
     });
 
-    this.getEvent().then(
-      ()=>{this.isDataAvailable = true;},
-      ()=>{console.log('rejected promise');}
-    );
   }
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
 
-  getEvent() {
-    this.eventPromise = new Promise<Event>((resolve, reject) => {
-      this.eventService.get(this.event_id)
-        .subscribe((data: Event) => {
-          this.event = data;
-          resolve();
-        }, (error) => {
-          console.log(error);
-          reject(error);
-        });
-
-    });
-    return this.eventPromise;
+  prettyDate(date_str: string){
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let date = new Date(date_str);
+    return date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
   }
 
 }
