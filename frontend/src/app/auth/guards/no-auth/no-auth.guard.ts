6eac0be9878eaf4cdfc3ac7a91aec590544a03b2
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-11-03 21:21:10 +0300

    feat(routing): automatically redirect to feed on no-auth pages

--- frontend/src/app/auth/guards/no-auth/no-auth.guard.ts
+++ frontend/src/app/auth/guards/no-auth/no-auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AuthService } from '../../providers/auth/auth.service';
@@ -8,11 +8,15 @@ import { AuthService } from '../../providers/auth/auth.service';
   providedIn: 'root'
 })
 export class NoAuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return !this.authService.isAuthenticated();
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['feed']);
+      return false;
+    }
+    return true;
   }
 }
