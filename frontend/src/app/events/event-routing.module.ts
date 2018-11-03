d3458f466894fe546aa15708079bc867e6907a0a
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-11-03 20:08:50 +0300

    feat(routing): protect event pages with auth guard

--- frontend/src/app/events/event-routing.module.ts
+++ frontend/src/app/events/event-routing.module.ts
@@ -5,10 +5,12 @@ import { FeedPage } from './pages/feed/feed.page';
 import { EventPage } from './pages/event/event.page';
 import { EventCreatePage } from './pages/event-create/event-create.page';
 
+import { AuthGuard } from '../auth/guards/auth/auth.guard';
+
 const routes: Routes = [
-  { path: '', component: FeedPage },
-  { path: 'create', component: EventCreatePage },
-  { path: ':id', component: EventPage },
+  { path: '', component: FeedPage, canActivate: [ AuthGuard ] },
+  { path: 'create', component: EventCreatePage, canActivate: [ AuthGuard ] },
+  { path: ':id', component: EventPage, canActivate: [ AuthGuard ] },
 ];
 
 @NgModule({
