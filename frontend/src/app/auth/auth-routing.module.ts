26d984fefd11129b028d7d8d93d77268cd8faa00
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-11-03 20:03:34 +0300

    feat(routing): protect signin & signup pages with no-auth guard

--- frontend/src/app/auth/auth-routing.module.ts
+++ frontend/src/app/auth/auth-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { SigninPage } from './pages/signin/signin.page';
 import { SignupPage } from './pages/signup/signup.page';
 
+import { NoAuthGuard } from './guards/no-auth/no-auth.guard';
+
 const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: 'full' },
-  { path: 'signin', component: SigninPage },
-  { path: 'signup', component: SignupPage }
+  { path: 'signin', component: SigninPage, canActivate: [ NoAuthGuard ] },
+  { path: 'signup', component: SignupPage, canActivate: [ NoAuthGuard ] }
 ];
 
 @NgModule({
