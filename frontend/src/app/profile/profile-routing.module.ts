c9b3397c4c704da3f74da7b6863c67c64e656624
Serkan Özel <serkan.ozel@boun.edu.tr>
2018-11-04 22:27:18 +0300

    feat(profile): add initial profile page module

---
+++ frontend/src/app/profile/profile-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {ProfileLandingPage} from "./pages/profile-landing/profile-landing.page";
+
+
+const routes: Routes = [
+  { path: '', component: ProfileLandingPage},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProfileRoutingModule { }
