0465070a37082943e0ce897c74eec95231df64af
Serkan Özel <serkan.ozel@boun.edu.tr>
2018-11-08 21:58:01 +0300

    chore(cors): add missing semicolon in app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ var eventsRouter = require('./routes/events');
 
 var app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/www')));
