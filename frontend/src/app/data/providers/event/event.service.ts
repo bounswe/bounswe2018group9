4b16f6c63b08e1dec560afec92cd0bcd40f6a6d4
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-11-08 16:28:29 +0300

    fix(api): properly inject dependencies to the event service

--- frontend/src/app/data/providers/event/event.service.ts
+++ frontend/src/app/data/providers/event/event.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { DataService } from '../data/data.service';
 import { Event } from '../../../interfaces';
@@ -8,4 +9,8 @@ import { Event } from '../../../interfaces';
 })
 export class EventService extends DataService<Event> {
   protected api = 'events';
+
+  constructor(protected http: HttpClient) {
+    super(http);
+  }
 }
