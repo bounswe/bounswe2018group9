c2e6901e2e3bedfcf682df8d1ee2b7c68f1f4644
Serkan Özel <serkan.ozel@boun.edu.tr>
2018-11-09 23:32:12 +0300

    feat(event-create): add alert and error handling for event create page

--- frontend/src/app/events/pages/event-create/event-create.page.ts
+++ frontend/src/app/events/pages/event-create/event-create.page.ts
@@ -4,7 +4,8 @@ import { Router } from '@angular/router';
 
 import { EventService } from '../../../data/providers/event/event.service';
 
-import {LoadingController} from "@ionic/angular";
+import {AlertController, LoadingController} from "@ionic/angular";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-event-create',
@@ -13,7 +14,8 @@ import {LoadingController} from "@ionic/angular";
 })
 export class EventCreatePage implements OnInit {
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder, private eventService: EventService, private router: Router,private loadingController :LoadingController) {
+  constructor(private formBuilder: FormBuilder, private eventService: EventService, private router: Router,
+              private loadingController :LoadingController, private alertController: AlertController) {
     this.form = this.formBuilder.group({
       name: ['', [Validators.required,Validators.pattern('[a-zA-Z ]*')]],
       price: ['', [Validators.required,Validators.pattern('[0-9]*')]],
@@ -36,7 +38,7 @@ export class EventCreatePage implements OnInit {
           this.router.navigate(['/feed']);
         },
         error => {
-          console.log(error);
+          this.handleError(error)
         },() => {
           this.loadingController.dismiss();
         }
@@ -50,4 +52,33 @@ export class EventCreatePage implements OnInit {
     });
     return await loading.present();
   }
+  async presentAlert(errMessage ,backendError : boolean) {
+    let alert;
+    if(backendError){
+      alert = await this.alertController.create({
+        header: 'Wait..',
+        subHeader: 'You could not create the event.',
+        message: 'You get a backend error: ' + errMessage,
+        buttons: ['Close']
+      });
+    }else{
+      alert = await this.alertController.create({
+        header: 'Wait..',
+        subHeader: 'You could not create the event.',
+        message: 'You get a client error: ' + errMessage,
+        buttons: ['Close']
+      });
+    }
+    await alert.present();
+  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      this.presentAlert(error.error.message,false);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      this.presentAlert(`${JSON.stringify(error.error)}`, true);
+    }
+  };
 }
