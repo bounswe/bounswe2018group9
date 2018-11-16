e4aaeaa6d96bd6737fa0067f27af5b9338854a58
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-11-16 13:26:35 +0300

    feat(interfaces): attendance & attendances interface

--- frontend/src/app/interfaces/index.ts
+++ frontend/src/app/interfaces/index.ts
@@ -3,3 +3,4 @@ export { Event } from './event.interface';
 export { Media } from './media.interface';
 export { Vote, Votes } from './vote.interface';
 export { Comment, Comments } from './comment.interface';
+export { Attendance, Attendances } from './attendance.interface';
