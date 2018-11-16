0b5d4c4c647c4bb927fe08b207febfd6b7275b24
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-11-16 13:40:56 +0300

    feat(interfaces): extend event interface

--- frontend/src/app/interfaces/event.interface.ts
+++ frontend/src/app/interfaces/event.interface.ts
@@ -1,11 +1,25 @@
 import { Object } from './object.interface';
-import {User} from "./user.interface";
+import { User } from './user.interface';
+import { Media } from './media.interface';
+import { Votes } from './vote.interface';
+import { Comments } from './comment.interface';
+import { Attendances } from './attendance.interface';
+import { Location } from './location.interface';
 
 export interface Event extends Object {
-  owner?: string;
-  name?: string;
+  creator?: string | User;
+  title?: string;
   description?: string;
-  artists?: string;
-  date?: string;
-  price?: number;
+  artists?: [string];
+  tags?: [string];
+  media?: Media
+  date?: number;
+  location?: Location;
+  price?: {
+    amount: number;
+    currency: string;
+  };
+  votes?: Votes;
+  comments?: Comments;
+  attendances?: Attendances;
 }
