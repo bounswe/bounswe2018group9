68f2191505126f8bdc6962386e7bc5470eca9830
gumityolcu <36168341+gumityolcu@users.noreply.github.com>
2018-11-27 19:19:59 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e
 HOST="root@46.101.223.116"
 # GIT_REVISION=$(git rev-parse --short HEAD)
 
-# TODO: avoid node_modules
+# TODO: avoid  node_modules
 # rm -r backend/node_modules
 scp -r backend $HOST:~
 ssh $HOST "cd backend && killall node && npm install && nohup npm start < /dev/null && exit"
