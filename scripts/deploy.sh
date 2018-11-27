5139f4e32de11dc06dcb5ef7812d2126ca2f415e
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-11-28 01:02:31 +0300

    hope message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ HOST="root@46.101.223.116"
 
 # TODO: avoid  node_modules
 # rm -r backend/node_modules
-# scp -r backend $HOST:~
+scp -r backend $HOST:~
 ssh $HOST 'screen -XS "actopus:prod" quit; screen -dmS "actopus:prod" bash -c "cd ~/backend && npm install && sudo npm start"; exit'
