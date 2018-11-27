1b6c958b89abbe235515352800bb3ba6a395b6ca
gumityolcu <36168341+gumityolcu@users.noreply.github.com>
2018-11-27 14:22:21 +0300

    Create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+HOST="root@46.101.223.116"
+# GIT_REVISION=$(git rev-parse --short HEAD)
+
+# TODO: avoid node_modules
+# rm -r backend/node_modules
+scp -r backend $HOST:~
+ssh $HOST:~ && cd backend &&  npm install && nohup npm start
