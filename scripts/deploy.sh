0aff78463416eff2043fbcaf814fbc8a1f1d2b05
gumityolcu <36168341+gumityolcu@users.noreply.github.com>
2018-12-01 23:54:17 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ HOST="root@46.101.223.116"
 
 # TODO: do scp with tar for efficiency
 scp -r backend $HOST:~
-ssh $HOST 'screen -XS "actopus:prod" quit; screen -dmS "actopus:prod" bash -c "cd ~/backend && npm install && sudo npm start"; exit'
+ssh $HOST 'screen -XS "actopus:prod" quit; screen -dmS "actopus:prod" bash -c "killall node && cd ~/backend && npm install && sudo npm start"; exit'
