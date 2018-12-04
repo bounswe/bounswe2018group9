d0280099dfd13f3d2f95eb12196cb9a98889f0e5
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-12-04 13:27:32 +0300

    chore(ci): support both dev & prod deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 HOST="root@46.101.223.116"
 
+# set ENV
+if [ -n "$1"]; then
+  ENV="$1"
+else
+  echo "Assuming development deployment!"
+  ENV="dev"
+fi
+
 # TODO: do scp with tar for efficiency
-scp -r backend $HOST:~
-ssh $HOST 'killall node; screen -XS "actopus:prod" quit; screen -dmS "actopus:prod" bash -c "cd ~/backend && npm install && sudo npm run start:prod"; exit'
+scp -r backend $HOST:~/$ENV
+ssh $HOST 'killall node; screen -XS "actopus:$ENV" quit; screen -dmS "actopus:$ENV" bash -c "cd ~/$ENV/backend && npm install && sudo npm run start:$ENV"; exit'
