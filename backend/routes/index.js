b2860785ac2e8a41da8c39ce966c8453a7523d7f
Nazmican <nazmicancalik@gmail.com>
2018-12-05 16:04:38 +0300

    Added router for search

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,11 +7,12 @@ const authRouter = require('./auth');
 const eventsRouter = require('./events');
 const usersRouter = require('./users');
 const uploadRouter = require('./upload');
+const searchRouter = require('./search');
 
 // Register API routers
 router.use('/auth', authRouter);
 router.use('/events', passport.authenticate('jwt', {session: false}), eventsRouter);
 router.use('/users', passport.authenticate('jwt', {session: false}), usersRouter);
 router.use('/upload', passport.authenticate('jwt', {session: false}), uploadRouter);
-
+router.use('/api/search', passport.authenticate('jwt', {session: false}), searchRouter);
 module.exports = router;
