ee7fecf3dc850b2a39ef2a319f01fa1efe11a87d
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-12-11 00:24:23 +0300

    Added annotation data model skeleton

--- backend/models/Annotations.js
+++ backend/models/Annotations.js
@@ -3,7 +3,23 @@ var Schema = mongoose.Schema;
 
 
 var AnnotationSchema = new Schema({
-    _id: { type: Schema.ObjectId, auto: true }
+    _id: { type: Schema.ObjectId, auto: true },
+    '@context': { type: String, required:true, default:"http://www.w3.org/ns/anno.jsonld" },
+    'type': { type: String, required: true, default:"Annotation"},
+    body: {
+        type: BodySchema,
+        required: false
+    },
+    target: {
+        source: {
+            type: String,
+            required: true
+        },
+        selector: {
+            type: SelectorSchema,
+            required: false
+        }
+    }
 });
 
 module.exports = AnnotationSchema;
