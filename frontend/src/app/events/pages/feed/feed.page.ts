d95d168882026ebd98a08b80c37e4134a94ecfc9
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-12-11 00:15:09 +0300

    feat(ui): feed infinite-scroll
    Closes #252.

--- frontend/src/app/events/pages/feed/feed.page.ts
+++ frontend/src/app/events/pages/feed/feed.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
 import { Event } from '../../../interfaces';
 
 import { AuthService } from '../../../auth/providers/auth/auth.service';
@@ -11,26 +13,41 @@ import { EventService } from '../../../data/providers/event/event.service';
   styleUrls: ['./feed.page.scss'],
 })
 export class FeedPage implements OnInit {
-  events: Event[];
-  loadedEvents : boolean = false;
-  private eventSub : any;
+  private static count = 3;
 
-  constructor(private router: Router, private authService: AuthService, private eventService: EventService) { }
+  private skip = 0;
+  private events: Event[] = [];
+
+  constructor(private router: Router, private authService: AuthService, private eventService: EventService) {
+    this.load();
+  }
 
   ngOnInit() {
-    console.log(this.authService.getUserFromToken());
-    this.eventSub = this.eventService.get()
+
+  }
+
+  /**
+   * Loads feed entries and appends entries to the existing loaded events
+   * @param {number} start the start index of will-be-loaded events
+   * @param {number} count the desired count for the load batch
+   */
+  load(event: any = null, start: number = this.skip, count: number = FeedPage.count) {
+    this.eventService.get(null, { limit: count, skip: start })
       .subscribe((data: Event[]) => {
-        this.events = data;
-        this.loadedEvents = true;
+        this.events.push(...data);
+        this.skip += count;
+
+        if (event) { // finalize infinite-scroll animation
+          event.target.complete();
+        }
       }, error => {
-        console.log(error);
+        if (event) { // finalize infinite-scroll animation
+          event.target.complete();
+        }
+        // TODO: Display error!
       });
   }
 
-  ngOnDestroy(){
-    this.eventSub.unsubscribe();
-  }
   logout() {
     this.authService
       .logout()
