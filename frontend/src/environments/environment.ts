43e315b910d307f68e7c8133e441de949ed3b5ce
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-12-11 14:31:44 +0300

    chore(dependency): add google api key

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,10 +1,13 @@
+import { secret } from './environment.private';
+
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  endpoint: 'http://46.101.223.116:3000/api'
+  endpoint: 'http://46.101.223.116:3000/api',
+  googleApiKey: secret.googleApiKey
 };
 
 /*
