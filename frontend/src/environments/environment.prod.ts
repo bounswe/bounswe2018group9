c3155311be2aa479aae3197198b98d9756608b47
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-12-11 14:31:44 +0300

    chore(dependency): add google api key

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
+import { secret } from './environment.private';
+
 export const environment = {
   production: true,
-  endpoint: 'http://46.101.223.116/api'
+  endpoint: 'http://46.101.223.116/api',
+  googleApiKey: secret.googleApiKey
 };
