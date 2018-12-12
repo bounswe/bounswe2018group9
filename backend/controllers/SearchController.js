eccdff246d28269d012e462db7b824367ce24d01
Nazmican <nazmicancalik@gmail.com>
2018-12-12 23:53:57 +0300

    Changed the basic search structure to send results for both Events and Users

--- backend/controllers/SearchController.js
+++ backend/controllers/SearchController.js
@@ -6,37 +6,30 @@ const Event = require('../models/Event')
 const User = require('../models/User'); 
 
 function search(req,res,next) {
-    
     const searchQuery = req.query.search;
-    
-    // Are you searching for searching users or events?
-    // If searching for users.
-    const isUser = req.query.isUser;
-    if (isUser) {
-        const regexedSearchQuery = new RegExp(escapeRegex(searchQuery), 'gi');
-        User.find({ $text: { $search : regexedSearchQuery }})
-            .exec()
-            .then((users) => {
-                res.status(200);
-                res.send(users);
-            })
-            .catch((err)=> {
-                res.status(404);
-                res.send(err);
-            });
-    } else {
-        const regexedSearchQuery = new RegExp(escapeRegex(searchQuery), 'gi');
-        Event.find({ $text: { $search : regexedSearchQuery }})
-            .exec()
-            .then((events) => {
-                res.status(200);
-                res.send(events);
-            })
-            .catch((err)=> {
-                res.status(404);
-                res.send(err);
-            });
-    }
+    let response = {};
+
+    const regexedSearchQuery = new RegExp(escapeRegex(searchQuery), 'gi');
+    User.find({ $text: { $search : regexedSearchQuery }})
+        .exec()
+        .then((users) => {
+            response.users = users;
+            return Event.find({ $text: { $search : regexedSearchQuery }})
+                .exec();
+        })
+        .then((events) => {
+            response.events = events;
+            res.status(200);
+            res.send(response); 
+        })
+        .catch((err)=> {
+            res.status(404);
+            res.send(err);
+        });
+}
+
+function advancedSearch(req,res,next){
+    const searchQuery = req.query.search;
 }
 
 // Escapes the search query strings for bad input. 
