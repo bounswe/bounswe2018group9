5144082c5dda085f1e331c5062844c681b3aeaab
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-12-13 23:59:47 +0300

    Add validation for TextPositionSelectors

--- backend/controllers/AnnotationController.js
+++ backend/controllers/AnnotationController.js
@@ -3,7 +3,29 @@ const Annotation = require("../models/Annotation");
 
 function addAnnotation(req, res, next) {
   var annot = new Annotation(req.body);
-
+  console.log(JSON.stringify(annot.target[0].selector.refinedBy.start));
+  if(annot.target[0].selector)
+  {
+    if(annot.target[0].selector.type==="TextPositionSelector")
+    {
+      if(annot.target[0].selector.start>annot.target[0].selector.end)
+      {
+        res.status(500);
+        res.send("TextPositionSelector.start should be <= TextPositionSelector.end");
+      }
+    }
+    else if(annot.target[0].selector.refinedBy)
+    {
+      if(annot.target[0].selector.refinedBy.type==="TextPositionSelector")
+      {
+        if(annot.target[0].selector.refinedBy.start>annot.target[0].selector.refinedBy.end)
+        {
+          res.status(500);
+          res.send("TextPositionSelector.start should be <= TextPositionSelector.end");
+        }
+      }
+    }
+  }
   annot.save()
     .then((annot) => {
       res.status(200);
