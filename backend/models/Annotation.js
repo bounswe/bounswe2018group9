b96b2407fefa34b28d3c51b0d16ee9bc6ca6a32d
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-12-13 22:54:07 +0300

    POST endpoint works correctly. Integrated discriminators to Annotation data model

--- backend/models/Annotation.js
+++ backend/models/Annotation.js
@@ -9,62 +9,8 @@ var BodySchema = new Schema({
     language: {type: String, required: false} // consult https://www.w3.org/International/articles/language-tags/
 }, {_id: false});
 
-var FragmentSelector = new Schema({
-    'type':{
-        type:String,
-        default:"FragmentSelector",
-        required: true
-    },
-    conformsTo:{
-        type: String,
-        default: "http://www.w3.org/TR/media-frags/", //????????????????
-        required: false //required for FragmentSelector type selectors
-    },
-    value: {
-        type: String,
-        required: false //Required for XPathSelector and FragmentSelector type selectors
-    },
-   /* refinedBy: {
-        type:  TextPositionSelector,
-        required: false
-    }*/
-});
-
-var XPathSelector = new Schema({
-    'type':{
-        type: String,
-        default: "XPathSelector",
-        required: true
-    },
-    value: {
-        type: String,
-        required: true //Required for XPathSelector and FragmentSelector type selectors
-    },
-  /*  refinedBy: {
-        type: TextPositionSelector,
-        required: false
-    }*/
-});
-
-var TextPositionSelector = new Schema({
-    'type':{
-        type: String,
-        default: 'TextPositionSelector',
-        required: true
-    },
-    start: {
-        type: Number,
-        required: true //required for TextPositionSelector type selectors
-    },
-    end: {
-        type: Number,
-        required: true //required for TextPositionSelector type selectors
-    },
-   /* refinedBy: {
-        type: TextPositionSelector,
-        required: false
-    }*/
-}, {_id: false});
+var SelectorSchema= new Schema({},
+    { discriminatorKey: 'type', _id: false });
 
 //THIS SCHEMA MEANS WE ONLY ALLOW SPECIFICRESOURCES AS TARGETS!!
 var SpecificResourceSchema = new Schema({
@@ -75,10 +21,78 @@ var SpecificResourceSchema = new Schema({
     },
 
     selector:{
-        type: TextPositionSelector || XPathSelector,
+        type: SelectorSchema,
         required: true
+    },
+
+    refinedBy:{
+        type:[SelectorSchema],
+        required:false
     }
 });
+
+SpecificResourceSchema.path('selector').discriminator('TextPositionSelector', new Schema({
+    start: {
+        type: Number,
+        required: true //required for TextPositionSelector type selectors
+    },
+    end: {
+        type: Number,
+        required: true
+    }
+  }, { _id: false }));
+
+  SpecificResourceSchema.path('selector').discriminator('XPathSelector', new Schema({
+    value: {
+        type: String,
+        required: true //Required for XPathSelector and FragmentSelector type selectors
+    }
+  }, { _id: false }));
+
+  SpecificResourceSchema.path('selector').discriminator('FragmentSelector', new Schema({
+    conformsTo:{
+        type: String,
+        default: "http://www.w3.org/TR/media-frags/", //????????????????
+        required: true 
+    },
+    value: {
+        type: String,
+        required: true
+    }
+  }, { _id: false }));
+
+  SpecificResourceSchema.path('refinedBy').discriminator('TextPositionSelector', new Schema({
+    start: {
+        type: Number,
+        required: true //required for TextPositionSelector type selectors
+    },
+    end: {
+        type: Number,
+        required: true
+    }
+  }, { _id: false }));
+
+  SpecificResourceSchema.path('refinedBy').discriminator('XPathSelector', new Schema({
+    value: {
+        type: String,
+        required: true //Required for XPathSelector and FragmentSelector type selectors
+    }
+  }, { _id: false }));
+
+  SpecificResourceSchema.path('refinedBy').discriminator('FragmentSelector', new Schema({
+    conformsTo:{
+        type: String,
+        default: "http://www.w3.org/TR/media-frags/", //????????????????
+        required: true 
+    },
+    value: {
+        type: String,
+        required: true
+    }
+  }, { _id: false }));
+
+
+  
 
 var AnnotationSchema = new Schema({
     '@context': { type: String, required:true, default:"http://www.w3.org/ns/anno.jsonld" },
