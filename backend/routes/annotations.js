fa55efcf5defc861e9fcc830501b2e35b05c1e8c
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-12-14 01:22:48 +0300

    Delete endpoint

--- backend/routes/annotations.js
+++ backend/routes/annotations.js
@@ -10,5 +10,4 @@ router.get('/:id', AnnotationController.getAnnotation);
 router.put('/:id', AnnotationController.updateAnnotation);
 router.delete('/:id', AnnotationController.deleteAnnotation);
 
-
 module.exports = router;
