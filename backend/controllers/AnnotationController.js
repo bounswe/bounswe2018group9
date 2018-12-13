ab8b989aea4fcea99c4e11e6c51959ba2c6dd48b
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-12-13 21:34:32 +0300

    Implemented POST endpoint

--- backend/controllers/AnnotationController.js
+++ backend/controllers/AnnotationController.js
@@ -1,2 +1,20 @@
 const mongoose = require("mongoose");
-const Event = require("../models/Annotation");
+const Annotation = require("../models/Annotation");
+
+function addAnnotation(req, res, next) {
+  var annot = new Annotation(req.body);
+
+  annot.save()
+    .then((annot) => {
+      res.status(200);
+      res.send({annot});
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send(err);
+    });
+};
+
+module.exports = {
+  addAnnotation
+};
