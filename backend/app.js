1def572d64faa09ab2d5ca7d19838149d9a4116f
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-12-14 15:28:07 +0300

    chore(cors): configure cors

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,10 @@ const usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/www')));
