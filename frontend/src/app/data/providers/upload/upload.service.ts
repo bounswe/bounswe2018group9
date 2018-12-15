c32e29b30022200910f65bb88fad14518bb3d1b6
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-12-15 13:50:18 +0300

    feat(api): upload service

---
+++ frontend/src/app/data/providers/upload/upload.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/common/http';
+import { Subject, Observable } from 'rxjs';
+
+@Injectable()
+export class UploadService {
+  static api = '/upload';
+
+  constructor(private http: HttpClient) {}
+
+  upload(file: File): { progress: Observable<number>, response: Observable<HttpResponse<any>> } {
+    const formData: FormData = new FormData();
+      formData.append('file', file, file.name);
+
+      const req = new HttpRequest('POST', UploadService.api, formData, {
+        reportProgress: true
+      });
+      const progress = new Subject<number>();
+      const response = new Subject<HttpResponse<any>>();
+
+      this.http.request(req).subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress) {
+          // update progress
+          const percentDone = Math.round(100 * event.loaded / event.total);
+          progress.next(percentDone);
+        } else if (event instanceof HttpResponse) {
+          // complete progress
+          progress.complete();
+
+          // send response
+          response.next(event);
+          response.complete();
+        }
+      });
+
+      return { progress: progress, response: response };
+  }
+}
