ac1ccd1dc500a8ce14b64d05d4c15a0787fbd18c
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-12-15 22:31:45 +0300

    feat(native): retrieve file or blob from filesystem

--- frontend/src/app/native/providers/file/file.service.ts
+++ frontend/src/app/native/providers/file/file.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@angular/core';
 
+import { File as FileNative, Entry, FileEntry, IFile } from '@ionic-native/file/ngx';
+
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
 
-  constructor() {
+  constructor(private fileNative: FileNative) {}
 
+  // TODO: Refactor FileService 'get' pathway (File | Blob??)
+  async extract(path: string): Promise<File | Blob> {
+    let entry = await this.fileNative.resolveLocalFilesystemUrl(path);
+    if (entry.isFile) {
+      return new Promise<File | Blob>((resolve, reject) => {
+        (entry as FileEntry).file(file => {
+          var reader = new FileReader();
+          reader.onloadend = function (e) {
+            let blob = new Blob([reader.result], { type: file.type });
+            resolve(blob)
+          };
+          reader.onerror = function (e) {
+            reject(e);
+          }
+          reader.readAsArrayBuffer(file);
+        }, error => {
+          reject(error);
+        })
+      });
+    } else {
+      return null;
+    }
   }
 
   async get(type: string): Promise<File[]> {
