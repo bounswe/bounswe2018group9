e4b75a0d647bade67c4a72fbb4847e5e30b6588d
Nazmican <nazmicancalik@gmail.com>
2018-12-17 10:13:44 +0300

    Advanced search and location search

--- backend/controllers/SearchController.js
+++ backend/controllers/SearchController.js
@@ -28,14 +28,103 @@ function search(req,res,next) {
         });
 }
 
+function locationSearch(req,res,next) {
+    const latLower = req.query.latLower;
+    const latHigher = req.query.latHigher;
+    const longHigher = req.query.longLower;
+    const longLower = req.query.longLower;
+
+    Event.find({ location: { coordinates: { lat: { $gte: latLower, $lte: latHigher}, long:{ $gte: longLower, $lte: longHigher}}}})
+        .exec()
+        .then((events) => {
+            res.status(200);
+            res.send(events);
+        })
+        .catch((err) => {
+            res.status(500);
+            res.send(err);
+        });
+}
+
 function advancedSearch(req,res,next){
     const searchQuery = req.query.search;
+    const regexedSearchQuery = new RegExp(escapeRegex(searchQuery),'gi');
+
+    let response = {}
+    let searchParams = {}
+
+    // Get query params
+    // Location
+    const latLower = req.query.latLower;
+    const latHigher = req.query.latHigher;
+    const longHigher = req.query.longLower;
+    const longLower = req.query.longLower;
+
+    // Time
+    const beforeThan = req.query.beforeThan;
+    const afterThan = req.query.afterThan;
+
+    // Price
+    const lowPrice = req.query.lowPrice;
+    const highPrice = req.query.highPrice;
+    const currency = req.query.currency;
+
+    // Construct the search params object
+    if (latLower && latHigher && longHigher && longLower){
+        searchParams.location = { 
+            coordinates: { 
+                lat: { 
+                    $gte: latLower, 
+                    $lte: latHigher
+                }, 
+                long:{ 
+                    $gte: longLower, 
+                    $lte: longHigher
+                }
+            }
+        };
+    }
+
+    // Time
+    // TODO: Parse date correctly according to frontend input.
+    if (beforeThan && afterThan){
+        searchParams.date = { 
+            $gte: afterThan, 
+            $lte: beforeThan
+        };
+    }
+
+    // Price
+    if (lowPrice && highPrice && currency){
+        searchParams.price = {
+            amount: {
+                $gte: afterThan, 
+                $lte: beforeThan
+            },
+            currency: currency
+        };
+    }
+
+    // Finally get the events 
+    Event.find(searchParams)
+        .exec()
+        .then((events) => {
+            res.status(200);
+            res.send(events);
+        })
+        .catch((err) => {
+            res.status(500);
+            res.send(err);
+        });
 }
 
 // Escapes the search query strings for bad input. 
 function escapeRegex(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 }
+
 module.exports = {
-    search
+    search,
+    advancedSearch,
+    locationSearch
 }
