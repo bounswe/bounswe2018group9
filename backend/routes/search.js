434b82f159a849fce8f40c3cf3cfc01cdd96261c
Nazmican <nazmicancalik@gmail.com>
2018-12-17 10:25:21 +0300

    Added router for advanced search

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const SearchController = require('../controllers/SearchController');
 
 router.get('/', SearchController.search);
+router.get('advanced', SearchController.advancedSearch);
 
 module.exports = router;
