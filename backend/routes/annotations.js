beb5960cdf52a2b82487a9213b789e7b4885af2d
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-12-17 23:52:21 +0300

    Added endpoint to get annotations associated with a URL

--- backend/routes/annotations.js
+++ backend/routes/annotations.js
@@ -8,5 +8,6 @@ router.post('/', AnnotationController.addAnnotation);
 router.get('/:id', AnnotationController.getAnnotation);
 router.put('/:id', AnnotationController.updateAnnotation);
 router.delete('/:id', AnnotationController.deleteAnnotation);
+router.post('/pageAnnotations',AnnotationController.getAnnotationsofPage);
 
 module.exports = router;
