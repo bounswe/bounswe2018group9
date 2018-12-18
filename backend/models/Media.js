2c51831b8b492c0e5c7a1267cc9c5bffa3c48671
Oğuz Kaan Yüksel <okyksl@gmail.com>
2018-12-18 11:54:53 +0300

    refactor(models): revise media model

--- backend/models/Media.js
+++ backend/models/Media.js
@@ -1,41 +1,22 @@
-var mongoose = require('mongoose');
-//var mongoosePaginate = require("mongoose-paginate");
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+// var mongoosePaginate = require("mongoose-paginate");
+const Schema = mongoose.Schema;
 
 const MediaSchema = new Schema({
-    _id: { type: Schema.ObjectId, auto: true },
-
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-
-    link: {
-        type: String,
-        required: true
-    },
-
-    filename: {
-        type: String,
-        required: true
-    },
-
-    filesize: {
-        type: Number,//in KB
-        default: 0
-    },
-
-    annotations: {
-      //THIS IS A PLACE HOLDER DEFINITION
-        type: String,
-        default: ""
-    },
-    //0 for image, 1 for video
-    media_type:{
-        type: Number,
-        default: 0
-    }
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: Number,
+    enum: [0, 1], // '0' for images, '1' for videos
+    default: 0,
+  },
 });
 
 module.exports = MediaSchema;
