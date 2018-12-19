d55cd668c45471652ddb1fadb527d603d295722e
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-12-19 14:59:46 +0300

    Changed endpoint name

--- backend/app.js
+++ backend/app.js
@@ -51,8 +51,8 @@ app.use(express.static(path.join(__dirname, '/www')));
 
 // Register API routers.
 app.use('/api/auth', authRouter);
-app.use('/api/events', /*passport.authenticate('jwt', {session: false}),*/ eventsRouter);
-app.use('/api/users', /*passport.authenticate('jwt', {session: false}),*/ usersRouter);
+app.use('/api/events', passport.authenticate('jwt', {session: false}), eventsRouter);
+app.use('/api/users', passport.authenticate('jwt', {session: false}), usersRouter);
 app.use('/api/annotations',/* passport.authenticate('jwt', {session: false}),*/annotationsRouter);
 app.get('/*', function(req, res) {
   res.sendFile(__dirname + '/www/index.html');
--- backend/routes/annotations.js
+++ backend/routes/annotations.js
@@ -5,9 +5,10 @@ var router = express.Router();
 const AnnotationController = require('../controllers/AnnotationController');
 
 router.post('/', AnnotationController.addAnnotation);
+router.get('/',AnnotationController.getAnnotationsofPage);
 router.get('/:id', AnnotationController.getAnnotation);
 router.put('/:id', AnnotationController.updateAnnotation);
 router.delete('/:id', AnnotationController.deleteAnnotation);
-router.get('/page',AnnotationController.getAnnotationsofPage);
+
 
 module.exports = router;
