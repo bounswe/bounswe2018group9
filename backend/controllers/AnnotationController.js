f8b738fdbf36fc1a169c46263b8c488c1dfed07d
Nazmican Çalık <nazmicancalik@gmail.com>
2018-12-23 11:40:30 +0300

    Change error object

--- backend/controllers/AnnotationController.js
+++ backend/controllers/AnnotationController.js
@@ -7,7 +7,6 @@ function getAnnotationsofPage(req,res,next)
   Annotation.find({"target.source": req.query.url})
   .exec()
   .then((annotations)=>{
-    // Change this to full endpoint 
     let hrefPrefix = 'http://46.101.223.116/api/annotations/';
     let filteredAnnotations = _.map(annotations,(annotation) => {
       annotation.id = hrefPrefix + annotation._id;
@@ -29,25 +28,25 @@ function getAnnotationsofPage(req,res,next)
 }
 
 function addAnnotation(req, res, next) {
-  var annot = new Annotation(req.body);
-  if(annot.target[0].selector)
-  {
-    if(annot.target[0].selector.type==="TextPositionSelector")
-    {
-      if(annot.target[0].selector.start>annot.target[0].selector.end)
-      {
+  let annot = new Annotation(req.body);
+  if(annot.target[0].selector){
+    
+    if(annot.target[0].selector.type === "TextPositionSelector"){
+      if(annot.target[0].selector.start > annot.target[0].selector.end){
         res.status(500);
-        res.send("TextPositionSelector.start should be <= TextPositionSelector.end");
+        res.send({
+          err: 'TextPositionSelector.start should be <= TextPositionSelector.end';
+        });
       }
     }
-    else if(annot.target[0].selector.refinedBy)
-    {
-      if(annot.target[0].selector.refinedBy.type==="TextPositionSelector")
-      {
-        if(annot.target[0].selector.refinedBy.start>annot.target[0].selector.refinedBy.end)
-        {
+    else if(annot.target[0].selector.refinedBy){
+      if(annot.target[0].selector.refinedBy.type === "TextPositionSelector"){
+        
+        if(annot.target[0].selector.refinedBy.start > annot.target[0].selector.refinedBy.end){
           res.status(500);
-          res.send("TextPositionSelector.start should be <= TextPositionSelector.end");
+          res.send({
+            err: 'TextPositionSelector.start should be <= TextPositionSelector.end';
+          });
         }
       }
     }
@@ -63,9 +62,9 @@ function addAnnotation(req, res, next) {
   });
 };
 
-function getAnnotation(req, res, next)
-{
-  var annotId=req.params.id;
+function getAnnotation(req, res, next){
+  let annotId = req.params.id;
+  
   Annotation.findById(annotId)
   .exec()
   .then((annot)=>{
@@ -80,15 +79,14 @@ function getAnnotation(req, res, next)
 
 function updateAnnotation(req, res, next)
 {
-  var annotId=req.params.id;
-  var newAnnot=req.body;
+  let annotId=req.params.id;
+  let newAnnot=req.body;
 
   newAnnot.modified=now();
   Annotation.findByIdAndUpdate(annotId,{ $set: newAnnot }, { new: true })
   .exec()
   .then((annot)=>{
     res.status(200);
-    
     res.send({updatedAnnotation: annot})
   })
   .catch((err)=>{
