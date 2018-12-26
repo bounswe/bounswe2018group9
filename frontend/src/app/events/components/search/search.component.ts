03facf15ab412f5900efa55d7e349bba70f9169b
kemaltulum <tulum.ke@gmail.com>
2018-12-26 17:16:58 +0300

    frontend(search): Remove tabs from search page

--- frontend/src/app/events/components/search/search.component.ts
+++ frontend/src/app/events/components/search/search.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-search',
+  selector: 'app-search-form',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
