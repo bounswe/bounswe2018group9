4b0c1d48d42eb602534dd64c28b18b27bc1a26d9
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-12-31 02:55:38 +0300

    Change authentication stages

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -6,35 +6,34 @@ const EventController = require('../controllers/EventController');
 
 router.get('/', EventController.getAllEvents);
 
-router.post('/', EventController.addEvent);
+router.post('/', passport.authenticate('jwt', {session: false}), EventController.addEvent);
 
 // Media endpoints
 router.post('/:id/media', EventController.addMedia);
 router.get('/:id/media', EventController.getMedia);
-router.put('/:id/media/:mediaId', EventController.updateMedia);
-router.delete('/:id/media/:mediaId', EventController.deleteMedia);
+router.put('/:id/media/:mediaId', passport.authenticate('jwt', {session: false}), EventController.updateMedia);
+router.delete('/:id/media/:mediaId', passport.authenticate('jwt', {session: false}), EventController.deleteMedia);
 
 
 // Adds an attendance to an event
-router.post('/:id/attendance', EventController.addAttendance);
-router.get('/:id/attendance', EventController.getAttendance);
-router.put('/:id/attendance', EventController.updateAttendance);
+router.post('/:id/attendance', passport.authenticate('jwt', {session: false}), EventController.addAttendance);
+router.get('/:id/attendance', passport.authenticate('jwt', {session: false}), EventController.getAttendance);
+router.put('/:id/attendance', passport.authenticate('jwt', {session: false}), EventController.updateAttendance);
 
 //Adds a vote to event
-router.post('/:id/vote', EventController.vote);
-router.post('/:id/unvote', EventController.unvote);
+router.post('/:id/vote', passport.authenticate('jwt', {session: false}), EventController.vote);
+router.post('/:id/unvote', passport.authenticate('jwt', {session: false}), EventController.unvote);
 router.get('/:id/getVotes', EventController.getVotes);
 
 
 // these should be last in code
-router.put('/:id', EventController.updateEvent);
+router.put('/:id', passport.authenticate('jwt', {session: false}), EventController.updateEvent);
 router.get('/:id', EventController.getEventbyId);
-router.delete('/:id', EventController.deleteEvent);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), EventController.deleteEvent);
 
-// Comments for event 
-router.post('/:id/comments', EventController.addComment);
+router.post('/:id/comments', passport.authenticate('jwt', {session: false}), EventController.addComment);
 router.get('/:id/comments', EventController.getComments);
-router.delete('/:id/comments/:commentId', EventController.deleteComment);
-router.put('/:id/comments/:commentId', EventController.updateComment);
+router.delete('/:id/comments/:commentId', passport.authenticate('jwt', {session: false}), EventController.deleteComment);
+router.put('/:id/comments/:commentId', passport.authenticate('jwt', {session: false}), EventController.updateComment);
 
 module.exports = router;
