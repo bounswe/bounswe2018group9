4dd5d8e27766869901ea070215291106a1061382
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-12-31 02:55:38 +0300

    Change authentication stages

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -14,12 +14,9 @@ const feedRouter = require('./feed');
 
 // Register API routers
 router.use('/auth', authRouter);
-router.use('/events', /*passport.authenticate('jwt', {session: false}),*/ eventsRouter);
-router.use('/users', /*passport.authenticate('jwt', {session: false}),*/ usersRouter);
-router.use('/upload', passport.authenticate('jwt', {session: false}), uploadRouter);
-router.use('/search', /*passport.authenticate('jwt', {session: false}),*/ searchRouter);
-router.use('/annotations', passport.authenticate('jwt', {session: false}), annotationsRouter);
-router.use('/utils', passport.authenticate('jwt', {session: false}), utilsRouter);
-router.use('/feed',passport.authenticate('jwt', {session: false}), feedRouter);
-
+router.use('/events', eventsRouter);
+router.use('/users', usersRouter);
+router.use('/upload', uploadRouter);
+router.use('/api/search', searchRouter);
+router.use('/annotations', annotationsRouter);
 module.exports = router;
