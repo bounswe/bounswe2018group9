9421404f2429eb8e454a906c81a9490a2823d1a5
Galip Ümit Yolcu <gumityolcu@gmail.com>
2018-12-31 03:08:35 +0300

    Fix bug

--- backend/routes/annotations.js
+++ backend/routes/annotations.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 //Import Controllers
 const AnnotationController = require('../controllers/AnnotationController');
