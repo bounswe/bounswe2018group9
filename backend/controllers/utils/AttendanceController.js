0a329c6ddc89f9e1f65508a2dea8b0de952a9d0a
Nazmican Çalık <nazmicancalik@gmail.com>
2019-01-01 22:39:26 +0300

    Added will not attend controller

--- backend/controllers/utils/AttendanceController.js
+++ backend/controllers/utils/AttendanceController.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 
 const _ = require("lodash");
 
-function attend(req,res,next){
+function willAttend(req,res,next){
     let userId = req.params.id;
     let eventId = req.params.eventId;
 
@@ -46,6 +46,49 @@ function attend(req,res,next){
         });
 }
 
+function willNotAttend(req,res,next){
+    let userId = req.params.id;
+    let eventId = req.params.eventId;
+
+    User.findById(userId)
+        .exec()
+        .then((user) => {
+            let newUser = user;
+            
+            // Subtract the event if it is in one of the other lists.
+
+            user.willAttendEvents = _.filter(user.willAttendEvents, (element) => {
+                return !element.eventId === eventId;
+            });
+
+            user.willNotAttendEvents = _.filter(user.willNotAttendEvents, (element) => {
+                return !element.eventId === eventId;
+            });
+
+            user.mayAttendEvents = _.filter(user.mayAttendEvents, (element) => {
+                return !element.eventId === eventId;
+            });
+
+            // Now add it to the new place
+
+            let newAttendance = {
+                eventId: mongoose.Types.ObjectId(eventId)
+            }
+
+            user.willNotAttendEvents.push(newAttendance);
+            return user.save();
+        })
+        .then((user) => {
+            res.status(200);
+            res.send({willNotAttendEvents: user.willNotAttendEvents});
+        })
+        .catch((err) => {
+            res.status(500);
+            res.send(err);
+        });
+}
+
 module.exports = {
-    attend
+    willAttend,
+    willNotAttend,
 }
