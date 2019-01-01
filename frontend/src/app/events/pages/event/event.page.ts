d17a192e2a417e518e293afdf5219ea6d080ad5d
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-01-02 00:21:47 +0300

    feat(event): finalize event detail annotation

--- frontend/src/app/events/pages/event/event.page.ts
+++ frontend/src/app/events/pages/event/event.page.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {Event, User, Comment, Attendance} from '../../../interfaces';
 import {ActivatedRoute, Router} from '@angular/router';
 import { EventService } from '../../../data/providers/event/event.service';
@@ -30,7 +30,6 @@ export class EventPage implements OnInit, OnDestroy, AfterViewInit{
   event_id: string;
   commentsSec = false;
 
-  annotations : HighlightTag[] = [];
   dateAnnotations : HighlightTag[] = [];
   priceAnnotations : HighlightTag[]=[];
   organizerAnnotations : HighlightTag[]=[];
@@ -39,14 +38,14 @@ export class EventPage implements OnInit, OnDestroy, AfterViewInit{
   locationAnnotations : HighlightTag[]=[];
   descriptionAnnotations : HighlightTag[]=[];
   tagAnnotations : HighlightTag[]=[];
-  @ViewChild('dateAnnotate') dateRef :ElementRef;
-  @ViewChild('priceAnnotate') priceRef :ElementRef;
-  @ViewChild('organizerAnnotate') organizerRef :ElementRef;
-  @ViewChild('artistAnnotate') artistRef :ElementRef;
-  @ViewChild('durationAnnotate') durationRef : ElementRef;
-  @ViewChild('locationAnnotate') locationRef :ElementRef;
-  @ViewChild('descriptionAnnotate') descriptionRef :ElementRef;
-  @ViewChild('tagAnnotate') tagRef :ElementRef;
+  @ViewChildren('dateAnnotate') dateRef : QueryList<ElementRef>;
+  @ViewChildren('priceAnnotate') priceRef : QueryList<ElementRef>;
+  @ViewChildren('organizerAnnotate') organizerRef : QueryList<ElementRef>;
+  @ViewChildren('artistAnnotate') artistRef : QueryList<ElementRef>;
+  @ViewChildren('durationAnnotate') durationRef : QueryList<ElementRef>;
+  @ViewChildren('locationAnnotate') locationRef : QueryList<ElementRef>;
+  @ViewChildren('descriptionAnnotate') descriptionRef : QueryList<ElementRef>;
+  @ViewChildren('tagAnnotate') tagRef : QueryList<ElementRef>;
   xpaths = {};
 
   constructor(private route: ActivatedRoute,
@@ -121,28 +120,77 @@ export class EventPage implements OnInit, OnDestroy, AfterViewInit{
         fragment => {
           if (fragment && fragment === 'comments') {
             this.content.scrollToBottom();
-            console.log(this.content);
+            console.  log(this.content);
           }
         });
-    if(this.dateRef)
-      this.xpaths['date'] = this.getElementTreeXPath(this.dateRef.nativeElement);
-    if(this.priceRef)
-      this.xpaths['price'] = this.getElementTreeXPath(this.priceRef.nativeElement);
-    if(this.organizerRef)
-      this.xpaths['organizer'] = this.getElementTreeXPath(this.organizerRef.nativeElement);
-    if(this.artistRef)
-      this.xpaths['artist'] = this.getElementTreeXPath(this.artistRef.nativeElement);
-    if(this.durationRef)
-      this.xpaths['duration'] = this.getElementTreeXPath(this.durationRef.nativeElement);
-    if(this.locationRef)
-      this.xpaths['location'] = this.getElementTreeXPath(this.locationRef.nativeElement);
-    if(this.descriptionRef)
-      this.xpaths['description'] = this.getElementTreeXPath(this.descriptionRef.nativeElement);
-    if(this.tagRef)
-      this.xpaths['tag'] = this.getElementTreeXPath(this.tagRef.nativeElement);
 
-    this.addAnnotations();
+    this.dateRef.changes.subscribe((comps: QueryList<ElementRef>) =>
+    {
+      if(comps.first && comps.first.nativeElement){
+        console.log('Loaded xpath of date');
+        this.xpaths['date'] = this.getElementTreeXPath(comps.first.nativeElement);
+      }
+    });
+    this.priceRef.changes.subscribe((comps: QueryList<ElementRef>) =>
+    {
+      if(comps.first && comps.first.nativeElement){
+        console.log('Loaded xpath of price');
+        this.xpaths['price'] = this.getElementTreeXPath(comps.first.nativeElement);
+      }
+
+    });
+    this.organizerRef.changes.subscribe((comps: QueryList<ElementRef>) =>
+    {
+      if(comps.first && comps.first.nativeElement){
+        console.log('Loaded xpath of organizer');
+        this.xpaths['organizer'] = this.getElementTreeXPath(comps.first.nativeElement);
+      }
+
+    });
+    this.artistRef.changes.subscribe((comps: QueryList<ElementRef>) =>
+    {
+      if(comps.first && comps.first.nativeElement){
+        console.log('Loaded xpath of artist');
+        this.xpaths['artist'] = this.getElementTreeXPath(comps.first.nativeElement);
+      }
+
+    });
+    this.durationRef.changes.subscribe((comps: QueryList<ElementRef>) =>
+    {
+      if(comps.first && comps.first.nativeElement){
+        console.log('Loaded xpath of duration');
+        this.xpaths['duration'] = this.getElementTreeXPath(comps.first.nativeElement);
+      }
+
+    });
+    this.locationRef.changes.subscribe((comps: QueryList<ElementRef>) =>
+    {
+      if(comps.first && comps.first.nativeElement){
+        console.log('Loaded xpath of location');
+        this.xpaths['location'] = this.getElementTreeXPath(comps.first.nativeElement);
+      }
+
+    });
+    this.descriptionRef.changes.subscribe((comps: QueryList<ElementRef>) =>
+    {
+      if(comps.first && comps.first.nativeElement){
+        console.log('Loaded xpath of desc');
+        this.xpaths['description'] = this.getElementTreeXPath(comps.first.nativeElement);
+      }
+
+    });
+    this.tagRef.changes.subscribe((comps: QueryList<ElementRef>) =>
+    {
+      if(comps.first && comps.first.nativeElement){
+        console.log('Loaded xpath of tag');
+        this.xpaths['tag'] = this.getElementTreeXPath(comps.first.nativeElement);
+      }
+    });
+
     //setTimeout(this.goToComments(),2000)
+    setTimeout(()=>{
+      this.addAnnotations()
+    },3000);
   }
 
   goToComments(){
@@ -219,7 +267,6 @@ export class EventPage implements OnInit, OnDestroy, AfterViewInit{
 
 
 addAnnotations(){
-
   this.annotationService.getAnnotationsByPage(this.router.url).subscribe((next)=>{
     if(Array.isArray(next["annotations"])){
       while(next["annotations"].length  != 0 ){
@@ -228,7 +275,7 @@ addAnnotations(){
         this.auth.getUserData(annotation.creator).subscribe((next : User)=>{
           if(annotation.target[0].selector.refinedBy && (annotation.target[0].selector.refinedBy.start == 0 || annotation.target[0].selector.refinedBy.start) &&
             annotation.target[0].selector.refinedBy.end){
-            this.annotations.push({
+            let annot = {
               indices: {
                 start: annotation.target[0].selector.refinedBy.start,
                 end: annotation.target[0].selector.refinedBy.end
@@ -240,65 +287,38 @@ addAnnotations(){
                 profileImage : ((next.images && next.images.avatar) ? next.images.avatar: undefined),
                 target: annotation.target[0].selector.value
               }
-            });
+            };
+            switch(annot.data.target) {
+              case this.xpaths['date']:
+                this.dateAnnotations.push(annot);
+                break;
+              case this.xpaths['price']:
+                this.priceAnnotations.push(annot);
+                break;
+              case this.xpaths['organizer']:
+                this.organizerAnnotations.push(annot);
+                break;
+              case this.xpaths['artist']:
+                this.artistAnnotations.push(annot);
+                break;
+              case this.xpaths['location']:
+                this.locationAnnotations.push(annot);
+                break;
+              case this.xpaths['duraton']:
+                this.durationAnnotations.push(annot);
+                break;
+              case this.xpaths['description']:
+                this.descriptionAnnotations.push(annot);
+                break;
+              case this.xpaths['tag']:
+                this.tagAnnotations.push(annot);
+                break;
+            }
           }else{
             console.log('An annotation does not have start and end fields.');
           }
         });
-
       }
-    }else{
-      let annotation  : Annotation = next;
-      let color = (annotation.creator == this.authService.getUserId()) ? 'bg-pink' : 'bg-blue';
-      this.auth.getUserData(annotation.creator).subscribe((next : User)=>{
-        if(annotation.target[0].selector.refinedBy && annotation.target[0].selector.refinedBy.start && annotation.target[0].selector.refinedBy.end){
-          this.annotations.push({
-            indices: {
-              start: annotation.target[0].selector.refinedBy.start,
-              end: annotation.target[0].selector.refinedBy.end
-            },
-            cssClass: color,
-            data: {
-              value : annotation.body[0].value,
-              user: {name: next.name,id :next._id},
-              profileImage : ((next.images && next.images.avatar)  ? next.images.avatar: undefined),
-              target: annotation.target[0].selector.value
-            }
-          });
-        }else{
-          console.log('An annotation does not have start and end fields.');
-        }
-      });
-
-    }
-  });
-  this.annotations.forEach(annotation => {
-    switch(annotation.data.target) {
-      case this.xpaths['date']:
-        this.dateAnnotations.push(annotation);
-        break;
-      case this.xpaths['price']:
-        this.priceAnnotations.push(annotation);
-        break;
-      case this.xpaths['organizer']:
-        this.organizerAnnotations.push(annotation);
-        break;
-      case this.xpaths['artist']:
-        this.artistAnnotations.push(annotation);
-        break;
-      case this.xpaths['location']:
-        this.locationAnnotations.push(annotation);
-        break;
-      case this.xpaths['duraton']:
-        this.durationAnnotations.push(annotation);
-        break;
-      case this.xpaths['description']:
-        this.descriptionAnnotations.push(annotation);
-        break;
-      case this.xpaths['tag']:
-        this.tagAnnotations.push(annotation);
-        break;
-      default:
     }
   });
 
