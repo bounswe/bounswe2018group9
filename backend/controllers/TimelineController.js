5e824f007732b8aa566787ab7a745159039031fd
Nazmican Çalık <nazmicancalik@gmail.com>
2019-01-01 19:57:47 +0300

    Added get own events endpoint for timeline

--- backend/controllers/TimelineController.js
+++ backend/controllers/TimelineController.js
@@ -4,6 +4,42 @@ const User = require("../models/User");
 
 const _ = require('lodash');
 
+function getOwnEvents(req,res,next)
+{
+    let skip, limit;
+    let userId = req.params.id;
+
+    if (!req.query.skip){
+        skip = 0;
+    } else {
+        skip = Number(req.query.skip);
+    }
+
+    if(!req.query.limit)
+    {
+        limit = 0;
+    } else {
+        limit = Number.req.query.limit;
+    }
+
+    Event.paginate(
+        {"creator": userId}, 
+        {offset: skipVar, limit: limitVar})
+    .then((result) => {
+      if (!result.docs){
+        res.status(404);
+        res.send('No event found with creator id: ' + userId);
+      } else {
+        res.status(200);
+        res.send(result.docs);
+      }
+    })
+    .catch((err)=>{
+      res.status(500);
+      res.send({err})
+    });
+}
+
 module.exports = {
-    
+    getOwnEvents
 };
