3e02843a32aad63fa81a90332890b944a1887522
Oğuz Kaan Yüksel <okyksl@gmail.com>
2019-01-02 01:36:01 +0300

    feat(ui): tag/preference selector

---
+++ frontend/src/app/events/components/tag-selector/tag-selector.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TagSelectorComponent } from './tag-selector.component';
+
+describe('TagSelectorComponent', () => {
+  let component: TagSelectorComponent;
+  let fixture: ComponentFixture<TagSelectorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TagSelectorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TagSelectorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- frontend/src/app/events/events.module.ts
+++ frontend/src/app/events/events.module.ts
@@ -15,9 +15,11 @@ import { CommentBoxComponent } from './components/comment-box/comment-box.compon
 import {SharedModule} from "../shared/shared.module";
 
 import { EventFormComponent } from './components/event-form/event-form.component';
-import {SearchPage} from "./pages/search/search.page";
+import { SearchPage } from './pages/search/search.page';
 import {EventEditPage} from "./pages/event-edit/event-edit.page";
 import {AnnotationModule} from '../annotation/annotation.module';
+
+import { TagSelectorComponent } from './components/tag-selector/tag-selector.component';
 
 @NgModule({
   imports: [
@@ -29,6 +31,9 @@ import {AnnotationModule} from '../annotation/annotation.module';
     AnnotationModule,
     SharedModule
   ],
+  entryComponents: [
+    TagSelectorComponent
+  ],
   declarations: [
     FeedPage,
     EventPage,
@@ -38,7 +43,8 @@ import {AnnotationModule} from '../annotation/annotation.module';
     SearchComponent,
     CommentBoxComponent,
     EventFormComponent,
-    EventEditPage
+    EventEditPage,
+    TagSelectorComponent
   ]
 })
 export class EventsModule { }
