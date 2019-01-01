df05cb94a9d9e539ac294c76911c9d27f9b299d0
Nazmican Çalık <nazmicancalik@gmail.com>
2019-01-01 22:59:40 +0300

    path totfix

--- backend/controllers/utils/AttendanceController.js
+++ backend/controllers/utils/AttendanceController.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const Event = require("../models/Event");
-const User = require("../models/User");
+const Event = require("../../models/Event");
+const User = require("../../models/User");
 
 const _ = require("lodash");
 
