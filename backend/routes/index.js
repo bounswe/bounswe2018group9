a855ced5f3e37dffe354b76efaf1ca058f03f582
Galip Ümit Yolcu <gumityolcu@gmail.com>
2019-01-02 16:07:14 +0300

    Fix location search

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -17,7 +17,7 @@ router.use('/auth', authRouter);
 router.use('/events', /*passport.authenticate('jwt', {session: false}),*/ eventsRouter);
 router.use('/users', /*passport.authenticate('jwt', {session: false}),*/ usersRouter);
 router.use('/upload', passport.authenticate('jwt', {session: false}), uploadRouter);
-router.use('/search', passport.authenticate('jwt', {session: false}), searchRouter);
+router.use('/search', /*passport.authenticate('jwt', {session: false}),*/ searchRouter);
 router.use('/annotations', passport.authenticate('jwt', {session: false}), annotationsRouter);
 router.use('/utils', passport.authenticate('jwt', {session: false}), utilsRouter);
 router.use('/feed',passport.authenticate('jwt', {session: false}), feedRouter);
