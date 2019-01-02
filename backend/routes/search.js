7ad28b603b833471c091f3c41bc9b809c07743aa
Nazmican Çalık <nazmicancalik@gmail.com>
2019-01-02 16:49:27 +0300

    Added user search

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const SearchController = require('../controllers/SearchController');
 
 router.get('/', SearchController.search);
+router.get('/user', SearchController.userSearch);
 router.get('/advanced', SearchController.advancedSearch);
 
 module.exports = router;
