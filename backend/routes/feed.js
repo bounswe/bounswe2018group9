3875795174f89e86f6b191dfd9d43ed4ca09dfd2
Oğuz Kaan Yüksel <okyksl@gmail.com>
2019-01-06 23:39:26 +0300

    fix(routing): allow only users to request feed

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -1,9 +1,11 @@
-var express = require('express');
+const express = require('express');
+const passport = require('passport');
+
 var router = express.Router();
 
 //Import Controllers
 const FeedController = require('../controllers/FeedController');
 
-router.get('/:id', FeedController.getFeedForUserWithId);
+router.get('/:id', passport.authenticate('jwt', {session: false}), FeedController.getFeedForUserWithId);
 
 module.exports = router;
